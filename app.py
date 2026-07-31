1e16fc2ef170f662f023132d3af438db5f7c531e
ashmina-ashraf <ashminaashi291@gmail.com>
2026-08-01 03:57:58 +0530

    Updated app.py

--- app.py
+++ app.py
@@ -1,12 +1,29 @@
 from resume_reader_utils import read_resume
-import logging
 
+from llm_utils import get_match_score
+from job_list_utils import get_job_list, get_job_desc
+
+import logging
 logging.basicConfig(level=logging.INFO)
 
-RESUME_PATH = r"C:\Users\ashmi\OneDrive\Documents\GitHub\ScoreMatch\Resume.pdf"
+
+RESUME_PATH = r"C:\Users\ashmi\OneDrive\Documents\GitHub\ScoreMatch\artifacts\Resume.pdf"
+
 
 #Read Resume 
 resume_content = read_resume(resume_path=RESUME_PATH)
 logging.info("Fetched resume content.")
 logging.debug(resume_content)
 
+
+job_lists = get_job_list()
+for job in job_lists:
+    logging.info(f"Fetching job description for {job}")
+
+    job_desc = get_job_desc(job)
+    logging.info("Fetch successful!")
+    logging.debug(job_desc)
+
+    logging.info("Getting Match Score ..")
+    response = get_match_score(resume_content=resume_content,
+                               job_desc=job_desc)
